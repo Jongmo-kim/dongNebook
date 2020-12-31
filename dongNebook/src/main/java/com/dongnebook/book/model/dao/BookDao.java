a5210954d316f88ab2edaa5885092122677878b8
jongmo <wa1358@naver.com>
2020-12-31 11:17:56 +0900

    test

--- dongNebook/src/main/java/com/dongnebook/book/model/dao/BookDao.java
+++ dongNebook/src/main/java/com/dongnebook/book/model/dao/BookDao.java
@@ -2,11 +2,14 @@ package com.dongnebook.book.model.dao;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author 김종모
  *
  */
+
+@Repository
 public class BookDao {
 	@Autowired
 	private SqlSessionTemplate session;
