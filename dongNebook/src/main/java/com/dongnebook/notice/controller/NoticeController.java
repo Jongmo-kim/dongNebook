2dd1ecfedbe0743ccf00cd54389f449aacc6e52b
gyeong127 <mamiee030@gmail.com>
2021-01-04 16:13:39 +0900

    add noticeFrm

--- dongNebook/src/main/java/com/dongnebook/notice/controller/NoticeController.java
+++ dongNebook/src/main/java/com/dongnebook/notice/controller/NoticeController.java
@@ -2,6 +2,8 @@ package com.dongnebook.notice.controller;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.dongnebook.notice.model.service.NoticeService;
 import com.dongnebook.notice.model.vo.Notice;
 import com.dongnebook.notice.model.vo.NoticePageData;
+import com.dongnebook.user.model.vo.User;
 
 /**
  * @author 진수경
@@ -30,5 +33,20 @@ public class NoticeController {
 		model.addAttribute("pageNavi",npd.getPageNavi());
 		
 		return "notice/noticeList";
+	}
+	
+	@RequestMapping("/noticeFrm.do")
+	//로그인 했다는 가정하에 진행
+	public String noticeFrm(User u,Model model,HttpSession session) {
+		User loginUser = (User)session.getAttribute("loginUser");
+		model.addAttribute("u",loginUser);
+		return "notice/noticeFrm";
+	}
+	
+	@RequestMapping("/insertNotice.do")
+	public String insertNotice(Model model) {
+		model.addAttribute("msg", "공지사항 등록 성공");
+		model.addAttribute("loc", "/");
+		return "common/msg";
 	}
 }
