ef1bcdcfb5ed8bfd56f925812898ebaca63f8755
gyeong127 <mamiee030@gmail.com>
2021-01-11 10:10:47 +0900

    add calendar typeAlias

--- dongNebook/src/main/java/com/dongnebook/calendar/controller/CalendarController.java
+++ dongNebook/src/main/java/com/dongnebook/calendar/controller/CalendarController.java
@@ -1,10 +1,14 @@
 package com.dongnebook.calendar.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dongnebook.calendar.model.service.CalendarService;
+import com.dongnebook.calendar.model.vo.Calendar;
 
 @Controller
 @RequestMapping("/calendar")
@@ -13,7 +17,8 @@ public class CalendarController {
 	private CalendarService service;
 	
 	@RequestMapping("/calendarView.do")
-	public String calendarView() {
+	public String calendarView(Model model) {
+		ArrayList<Calendar> list = service.selectCalendarList();
 		return "calendar/calendarView";
 	}
 	
--- dongNebook/src/main/java/com/dongnebook/calendar/model/dao/CalendarDao.java
+++ dongNebook/src/main/java/com/dongnebook/calendar/model/dao/CalendarDao.java
@@ -1,11 +1,21 @@
 package com.dongnebook.calendar.model.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.dongnebook.calendar.model.vo.Calendar;
 
 @Repository
 public class CalendarDao {
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+
+	public ArrayList<Calendar> selectCalendarList() {
+		List<Calendar> list = sqlSession.selectList("calendar.selectCalendarList");
+		return (ArrayList<Calendar>)list;
+	}
 }
